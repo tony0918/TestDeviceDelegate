b3d719d7c4ebf283e901773297c2d09ded53814c
Tony Mao <huiliumao@gmail.com>
2014-08-25 15:57:42 +0800

    first commit

---
+++ src/testdevicedelegate/TestDeviceDelegate.java
@@ -0,0 +1,16 @@
+package testdevicedelegate;
+
+/**
+ *
+ * @author tony
+ */
+public class TestDeviceDelegate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
